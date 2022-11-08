1d43407806b9fb3ba9e777a5ba6344132683b530
Carlos Bartolo <carls940@gmail.com>
2022-11-08 09:09:28 -0500

    feature: defining routes and middleware

--- src/config/express.js
+++ src/config/express.js
@@ -1,3 +1,12 @@
 import express from 'express';
+import userRouter from '#Routes/user.routes.js';
+
 const expressApp = express();
+
+// Middlewares
+expressApp.use(express.json());
+
+// Routers
+expressApp.use('/user', userRouter);
+
 export default expressApp;
